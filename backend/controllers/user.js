b2d133b5a11e7c2a9eb65873851ddb2c4ce98a61
Simon <simon_froger@yahoo.fr>
2021-07-20 11:10:14 +0200

    Regex on user Email and Password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,6 +6,18 @@ require('dotenv').config
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
+  
+  const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if (!EMAIL_REGEX.test(req.body.email)){
+    return res.status(400)({ error: 'email non valide'})
+}
+
+//Mot de passe entre 4 et 8 charatères, il doit contenir au moins un chiffre
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
+if (!PASSWORD_REGEX.test(req.body.password)){
+    return res.status(400)({ error: 'Mot de passe non valide: il doit contenir entre 4 et 8 charactères et au moins un chiffre'})
+}
+
     bcrypt
       .hash(req.body.password, 10)
       .then((hash) => {
